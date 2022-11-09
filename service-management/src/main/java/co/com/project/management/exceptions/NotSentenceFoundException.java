4b53ffdad92464cac1722b342333bd514635379f
Jefry Zarate <jefryzarateruiz@gmail.com>
2022-11-08 20:42:15 -0500

    app

--- service-management/src/main/java/co/com/project/management/exceptions/NotSentenceFoundException.java
+++
@@ -1,9 +0,0 @@
-package co.com.project.management.exceptions;
-
-import co.com.project.common.exceptions.NotFoundException;
-
-public class NotSentenceFoundException extends NotFoundException {
-    public NotSentenceFoundException() {
-        super("Not found sentence in server");
-    }
-}
